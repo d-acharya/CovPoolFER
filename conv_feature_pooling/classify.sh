f3932361bda25ddf9cc86670aeff8df9a17b39a2
Dinesh Acharya <d-acharya@users.noreply.github.com>
2018-09-25 11:54:24 +0200

    Update classify.sh

--- conv_feature_pooling/classify.sh
+++ conv_feature_pooling/classify.sh
@@ -1,1 +1,3 @@
-python src/softmax_classifier.py CLASSIFY data/RAFDB/test/ models/20180501-012345 ~/pkl --batch_size=128 --image_size=100
+python src/classifit_rafdb.py TRAIN data/RAFDB/test/ models/<model-dir e.g. 20180501-012345> ~/*.pkl --batch_size=128 --image_size=100
+#pkl and model files below should be same as above
+#python src/classifit_rafdb.py CLASSIFY data/RAFDB/test/ models/<model-dir e.g. 20180501-012345> ~/*.pkl --batch_size=128 --image_size=100
